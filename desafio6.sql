289d2678c0a0a24bcabef3762c5369d24caa16df
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-09-28 10:15:20 -0300

    Resolve desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+	MIN(p.value) AS faturamento_minimo,
+    MAX(p.value) AS faturamento_maximo,
+    ROUND(AVG(p.value), 2) AS faturamento_medio,
+    SUM(p.value) AS faturamento_total
+FROM SpotifyClone.plans AS p
+INNER JOIN SpotifyClone.users AS u
+	ON p.plan_id = u.plan_id;
